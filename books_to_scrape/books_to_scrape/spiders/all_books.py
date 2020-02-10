f42253cf46562a062e0c7aa53fe489d748481f45
Luciano Galicki <lgalicki@gmail.com>
2020-02-10 19:58:30 -0300

    Added two examples of crawl spiders

---
+++ books_to_scrape/books_to_scrape/spiders/all_books.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+import scrapy
+from scrapy.linkextractors import LinkExtractor
+from scrapy.spiders import CrawlSpider, Rule
+
+
+class AllBooksSpider(CrawlSpider):
+    name = 'all_books'
+    allowed_domains = ['toscrape.com']
+    start_urls = ['http://books.toscrape.com/index.html']
+
+    rules = (
+        Rule(LinkExtractor(restrict_xpaths='//h3/a'),
+             callback='parse_item', follow=True),
+        Rule(LinkExtractor(restrict_xpaths='//li[@class="next"]/a'))
+    )
+
+    def parse_item(self, response):
+        print(response)
+        item = dict()
+        item['book_name'] = response.xpath('//h1/text()').get()
+        item['price'] = response.xpath('//p[@class="price_color"]/text()').get()
+        return item
